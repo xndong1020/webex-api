070a8a0b2ee6391dc9e266a4de20265cd9c2260b
zhuxchong <zhuxchong@gmail.com>
2019-04-02 11:14:32 +1100

    Header and Sidebar

---
+++ client/src/components/navigation/sidebar.js
@@ -0,0 +1,113 @@
+import React from "react";
+import PropTypes from "prop-types";
+import classNames from "classnames";
+import { withStyles } from "@material-ui/core/styles";
+
+import Drawer from "@material-ui/core/Drawer";
+
+import List from "@material-ui/core/List";
+
+import Divider from "@material-ui/core/Divider";
+import IconButton from "@material-ui/core/IconButton";
+
+import MenuIcon from "@material-ui/icons/Menu";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import { mainListItems } from "./listItem/listItems";
+import TestContext from "../../store/test-context";
+const drawerWidth = 240;
+
+const styles = theme => ({
+  root: {
+    display: "flex"
+  },
+  toolbar: {
+    paddingRight: 24 // keep right padding when drawer closed
+  },
+  toolbarIcon: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-end",
+    padding: "0 8px",
+    ...theme.mixins.toolbar
+  },
+  drawerPaper: {
+    position: "relative",
+    whiteSpace: "nowrap",
+    width: drawerWidth,
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen
+    })
+  },
+  drawerPaperClose: {
+    overflowX: "hidden",
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen
+    }),
+    width: theme.spacing.unit * 7,
+    [theme.breakpoints.up("sm")]: {
+      width: theme.spacing.unit * 9
+    }
+  },
+  appBarSpacer: theme.mixins.toolbar,
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing.unit * 3,
+    height: "100vh",
+    overflow: "auto"
+  },
+  chartContainer: {
+    marginLeft: -22
+  },
+  tableContainer: {
+    height: 320
+  },
+  h5: {
+    marginBottom: theme.spacing.unit * 2
+  }
+});
+
+class Sidebar extends React.Component {
+  static contextType = TestContext;
+
+  handleDrawerClose = () => {
+    this.context.closeShow();
+  };
+
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <Drawer
+        variant="permanent"
+        classes={{
+          paper: classNames(
+            classes.drawerPaper,
+            !this.context.open && classes.drawerPaperClose
+          )
+        }}
+        open={this.context.open}
+      >
+        <div className={classes.toolbarIcon}>
+          <IconButton onClick={this.handleDrawerClose}>
+            <ChevronLeftIcon />
+          </IconButton>
+        </div>
+        <Divider />
+        <List>{mainListItems}</List>
+        <Divider />
+      </Drawer>
+
+      // <main className={classes.content}>
+      //   <div className={classes.appBarSpacer} />
+      // </main>
+    );
+  }
+}
+
+Sidebar.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(Sidebar);
