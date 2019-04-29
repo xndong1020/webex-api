e978bc52421d651e4a473af164e3aca9ccb5a603
isdance <isdance2004@hotmail.com>
2019-04-29 19:42:27 +1000

    fix bugs of checkJwt middleware

--- api/app.js
+++ api/app.js
@@ -31,9 +31,8 @@ app.use(function(err, req, res, next) {
   res.locals.message = err.message
   res.locals.error = req.app.get('env') === 'development' ? err : {}
 
-  // render the error page
-  res.status(err.status || 500)
-  res.render('error')
+  // send error message
+  res.status(500).send(err.message)
 })
 
 module.exports = app
--- api/middlewares/checkJwt.js
+++ api/middlewares/checkJwt.js
@@ -4,22 +4,23 @@ const { JSON_TOKEN_SECRET } = require('../config')
 const checkJwt = (req, res, next) => {
   try {
     const authHeader = req.headers.authorization
+    if (!authHeader) return next(new Error('Auth header missing'))
+    const bits = authHeader.split(' ')
+    
+    if (!bits || bits.length !== 2) return next(new Error('Auth header wrong shape'))
+    
     const token = authHeader.split(' ')[1]
-
-    try {
-      const result = jwtwebtoken.verify(token, JSON_TOKEN_SECRET)
-      req.isAuthenticated = true
-      req.user = {
-        id: result.sub,
-        email: result.email
-      }
-      next()
-    } catch (e) {
-      req.isAuthenticated = false
-      next(e)
+    const result = jwtwebtoken.verify(token, JSON_TOKEN_SECRET)
+    
+    req.isAuthenticated = true
+    req.user = {
+      id: result.sub,
+      email: result.email
     }
+    next()
   } catch (e) {
     req.isAuthenticated = false
+    next(e)
   }
 }
 
